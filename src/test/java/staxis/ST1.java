baf668a1e88507721213addb7de1dabab532b46d
Zoltan Haindrich <zhaindrich@hortonworks.com>
2018-11-08 17:36:25 +0100

    trial

--- src/test/java/staxis/ST1.java
+++ src/test/java/staxis/ST1.java
@@ -1,5 +1,6 @@
 package staxis;
 
+import org.apache.commons.math3.special.Beta;
 import org.apache.commons.math3.stat.descriptive.moment.SecondMoment;
 import org.junit.Test;
 
@@ -28,6 +29,10 @@ public class ST1 {
     public double get2() {
       return m.get2();
     }
+
+    public double getN() {
+      return m.getN();
+    }
   }
 
   static class BetaDistributionOracle {
@@ -38,14 +43,24 @@ public class ST1 {
 
     public BetaDistributionOracle(MyMoment1 m2) {
       this.m2 = m2;
-      double mean = m2.get1();
-      double var = m2.get2();
+      double mean = map(m2.get1());
+      double var = m2.get2() / (m2.max - m2.min) / (m2.max - m2.min) / (m2.getN() - 1);
 
+      System.out.println(mean);
+      System.out.println(var);
       if (!(var < mean * (1 - mean))) {
         throw new RuntimeException("error");
       }
       a = mean * (mean * (1 - mean) / var - 1);
       b = (1 - mean) * (mean * (1 - mean) / var - 1);
+    }
+
+    private double map(double get1) {
+      return (get1 - m2.min) / (m2.max - m2.min);
+    }
+
+    private double estimateCDF(double x) {
+      return Beta.regularizedBeta(map(x), a, b);
     }
 
   }
@@ -69,14 +84,26 @@ public class ST1 {
     MyMoment1 m2 = new MyMoment1();
     //    new SeriesGenerator(1000).;
     int U = 0;
-    int V = 100;
-    for (int i = U; i <= V; i++)
-      m2.increment(i);
+    int V = 10;
+    int L = 70;
+    double cnt = 0;
+    for (int i = U; i <= V; i++) {
+
+      int d = i * i * i;// - (i - 50) * (i - 50);
+      if (d < L) {
+        cnt++;
+      }
+      m2.increment(d);
+    }
+    cnt /= V - U + 1;
 
     System.out.println(m2.get1());
     System.out.println(m2.get2());
 
-    new BetaDistributionOracle(m2);
+    BetaDistributionOracle bb = new BetaDistributionOracle(m2);
+    double ee = bb.estimateCDF(L);
+    System.out.println(ee);
+    System.out.println(cnt);
   }
 
 }
